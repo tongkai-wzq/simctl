582de5c8692c978f86e648d81eac16a6daea8f71
wuzhiqiang <332140686@qq.com>
2024-09-30 22:58:06 +0800

    gateway

--- controller/gwengine.go
+++ controller/gwengine.go
@@ -1,1 +1,28 @@
 package controller
+
+import (
+	"simctl/db"
+	"simctl/model"
+)
+
+type GatewayEngine struct {
+	lastId         int64
+	gatewayUser    model.GatewayUser
+	qryItems       []*geItem
+	qryFunsCounter map[string]int
+}
+
+func (ge *GatewayEngine) GetSims() []model.Sim {
+	var sims []model.Sim
+	if db.Engine.Where("gwuser_id = ? AND id > ?", ge.gatewayUser.Id, ge.lastId).OrderBy("id").Limit(10).Find(&sims); len(sims) == 0 {
+		ge.lastId = 0
+	} else {
+		ge.lastId = sims[len(sims)-1].Id
+	}
+	return sims
+}
+
+type geItem struct {
+	sim     model.Sim
+	qryFuns []string
+}
--- model/gwuser.go
+++ model/gwuser.go
@@ -2,43 +2,42 @@ package model
 
 import "simctl/gateway"
 
-var GateWays map[int64]gateway.GateWayer = make(map[int64]gateway.GateWayer)
-
 type GatewayUser struct {
 	Id       int64
 	Name     string            `json:"name"`
 	Operator string            `json:"operator"`
 	GwType   string            `json:"gwType"`
 	Params   map[string]string `xorm:"json" json:"params"`
+	gateway  gateway.GateWayer `xorm:"-" json:"-"`
 }
 
-func (gu *GatewayUser) BuildGateWay() gateway.GateWayer {
-	if gateway, ok := GateWays[gu.Id]; ok {
-		return gateway
+func (gu *GatewayUser) GateWay() gateway.GateWayer {
+	if gu.gateway != nil {
+		return gu.gateway
 	}
 	switch gu.GwType {
 	case "mobile":
-		GateWays[gu.Id] = &gateway.Mobile{
+		gu.gateway = &gateway.Mobile{
 			Appid:    gu.Params["appid"],
 			Password: gu.Params["password"],
 		}
-		GateWays[gu.Id].SetGwUserId(gu.Id)
+		gu.gateway.SetGwUserId(gu.Id)
 	case "unicom":
-		GateWays[gu.Id] = &gateway.Unicom{
+		gu.gateway = &gateway.Unicom{
 			AppId:     gu.Params["appId"],
 			AppSecret: gu.Params["appSecret"],
 			OpenId:    gu.Params["openId"],
 		}
-		GateWays[gu.Id].SetGwUserId(gu.Id)
+		gu.gateway.SetGwUserId(gu.Id)
 	case "telecom":
-		GateWays[gu.Id] = &gateway.Telecom{
+		gu.gateway = &gateway.Telecom{
 			AppKey:     gu.Params["appKey"],
 			SecretKey:  gu.Params["secretKey"],
 			CustNumber: gu.Params["custNumber"],
 		}
-		GateWays[gu.Id].SetGwUserId(gu.Id)
+		gu.gateway.SetGwUserId(gu.Id)
 	default:
 		return nil
 	}
-	return GateWays[gu.Id]
+	return gu.gateway
 }
