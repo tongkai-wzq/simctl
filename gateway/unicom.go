b0b2bedad0cf2008fad89bec5680ae088452b2a5
wuzhiqiang <332140686@qq.com>
2024-10-07 21:50:47 +0800

    sync_at

--- gateway/unicom.go
+++ gateway/unicom.go
@@ -30,19 +30,21 @@ func (u *Unicom) getParams() map[string]any {
 	msec := strconv.Itoa(now.Nanosecond() / 1000000)
 	transId := fmt.Sprintf("%v%v%v", now.Format("20060102150405"), msec, strconv.Itoa(rand.New(rand.NewSource(now.UnixNano())).Intn(900000)+100000))
 	params := map[string]any{
-		"app_id": u.AppId, "timestamp": now.Format("2006-01-02 15:04:05 ") + msec, "trans_id": transId,
+		"app_id":    u.AppId,
+		"timestamp": fmt.Sprintf("%v %v", now.Format("2006-01-02 15:04:05"), msec),
+		"trans_id":  transId,
 	}
 	var paramsStr string
 	for k, v := range params {
 		paramsStr = fmt.Sprintf("%v%v%v", paramsStr, k, v)
 	}
-	params["token"] = hex.EncodeToString(sm3.Sm3Sum([]byte(paramsStr + u.AppSecret)))
+	params["token"] = hex.EncodeToString(sm3.Sm3Sum([]byte(fmt.Sprintf("%v%v", paramsStr, u.AppSecret))))
 	return params
 }
 
 func (u *Unicom) post(uri string, data map[string]any, resp any) error {
 	params := u.getParams()
-	data["messageId"] = "gonet"
+	data["messageId"] = "simctl"
 	data["openId"] = u.OpenId
 	data["version"] = "1.0"
 	params["data"] = data
@@ -115,24 +117,25 @@ func (u *Unicom) QryDtls(simers []Simer) error {
 	} else if resp.Status != "0000" {
 		return fmt.Errorf("request wsGetTerminalDetails err : %v %v", resp.Status, resp.Message)
 	}
-	for _, simer := range simers {
-		for _, terminal := range resp.Data.Terminals {
-			if terminal.Iccid == simer.GetIccid() {
-				if status, err := strconv.Atoi(terminal.SimStatus); err == nil {
-					simer.SetStatus(int8(status))
-				}
-				if terminal.RealNameStatus == "2" || terminal.RealNameStatus == "3" {
-					simer.SetAuth(true)
-				} else {
-					simer.SetAuth(false)
-				}
-				if monthToDateUsage, err := strconv.ParseFloat(terminal.MonthToDateDataUsage, 64); err == nil {
-					simer.SetMonthKb(int64(monthToDateUsage * 1024))
-				}
-				simer.SetSyncAt()
-				db.Engine.Cols("status", "auth", "month_kb", "month_at", "sync_at").Update(simer)
-				break
+	for _, terminal := range resp.Data.Terminals {
+		for _, simer := range simers {
+			if terminal.Iccid != simer.GetIccid() {
+				continue
 			}
+			if status, err := strconv.Atoi(terminal.SimStatus); err == nil {
+				simer.SetStatus(int8(status))
+			}
+			if terminal.RealNameStatus == "2" || terminal.RealNameStatus == "3" {
+				simer.SetAuth(true)
+			} else {
+				simer.SetAuth(false)
+			}
+			if monthToDateUsage, err := strconv.ParseFloat(terminal.MonthToDateDataUsage, 64); err == nil {
+				simer.SetMonthKb(int64(monthToDateUsage * 1024))
+			}
+			simer.SetSyncAt()
+			db.Engine.Cols("status", "auth", "month_kb", "month_at", "sync_at").Update(simer)
+			break
 		}
 	}
 	return nil
