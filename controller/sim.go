1f4244fa0c15e6f27c22b6d7046379e7b2c0381e
wuzhiqiang <332140686@qq.com>
2024-10-09 17:57:47 +0800

    sim-list

--- controller/sim.go
+++ controller/sim.go
@@ -1,9 +1,32 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+	"simctl/db"
+	"simctl/model"
+
+	"github.com/go-chi/render"
+	"xorm.io/builder"
+)
+
+type simDetail struct {
+	Iccid   string `json:"iccid"`
+	Msisdn  string `json:"msisdn"`
+	MapNber string `json:"mapNber"`
+}
 
 func Sim(w http.ResponseWriter, r *http.Request) {
-
+	var cond builder.Cond
+	if simNber := r.URL.Query().Get("simNber"); simNber != "" {
+		cond = builder.Eq{"iccid": simNber}.Or(builder.Eq{"msisdn": simNber}).Or(builder.Eq{"map_nber": simNber})
+	}
+	var sim model.Sim
+	if has, err := db.Engine.Where(cond).Get(&sim); err == nil && has {
+		var detail simDetail
+		render.JSON(w, r, map[string]any{"code": 0, "detail": detail})
+	} else if err == nil {
+		render.JSON(w, r, map[string]any{"code": 4003, "msg": "未查询到"})
+	}
 }
 
 func SimList(w http.ResponseWriter, r *http.Request) {
